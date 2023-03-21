0cf968417f57986668b8fbad5d5da748b697b94c
NiaG <13niag17@gmail.com>
2023-03-21 20:55:01 +0300

    Hello favorite if-else

---
+++ HelloFavorite/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wou, this is Masha!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
